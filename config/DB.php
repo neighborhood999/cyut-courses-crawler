4b4acfe430cbe02db483898d994d64e65f8b0619
Peng-Jie <dean.leehom@gmail.com>
2015-12-02 22:41:21 +0800

    fix DB.php Tab -> Space

--- config/DB.php
+++ config/DB.php
@@ -6,28 +6,28 @@ use \PDO;
 
 class DB
 {
-	private $host = 'localhost';
-	private $username = 'Your Account';
-	private $password = 'Your Password';
-	private $dbName = 'Your DataBase';
-	private $db;
+    private $host = 'localhost';
+    private $username = 'Your Account';
+    private $password = 'Your Password';
+    private $dbName = 'Your DataBase';
+    private $db;
 
-	public function __construct()
-	{
-		try {
-			$this->db = new PDO(
-				"mysql:host=$this->host; dbname=$this->dbName; charset=utf8",
-				$this->username,
-				$this->password,
-				array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
-			);
-		} catch (PDOException $e) {
-			echo __LINE__ . ': ' . $e->getMessage();
-		}
-	}
+    public function __construct()
+    {
+        try {
+            $this->db = new PDO(
+                "mysql:host=$this->host; dbname=$this->dbName; charset=utf8",
+                $this->username,
+                $this->password,
+                array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION)
+            );
+        } catch (PDOException $e) {
+            echo __LINE__ . ': ' . $e->getMessage();
+        }
+    }
 
-	public function insertCourses($data, $config)
-	{
-		// ignore.
-	}
+    public function insertCourses($data, $config)
+    {
+        // ignore.
+    }
 }
