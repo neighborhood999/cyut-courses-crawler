2394c5cadfdb3c0db63a61699dc854f1bab3bc8d
Peng-Jie <dean.leehom@gmail.com>
2015-12-04 22:52:06 +0800

    Have two choose crawling courses.

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <?php
 require 'vendor/autoload.php';
+
 $config = require './config/config.example.php';
+$depCode = require './config/departmentCode.php';
 
 use Pengjie\CyutCrawler\CyutCourse;
 
+// Get single coures information.
 $cyut = new CyutCourse($config);
-
-$body = $cyut->getCourse()['body'];
+$body = $cyut->getSingleCoursesInfo()['body'];
 $filterBody = $cyut->crawlerResult($body);
 $result = $cyut->chunckResult($filterBody);
 var_dump($result);
+
+/*
+ * Get department 1 - 4 grade courses information.
+ *
+ * $cyut = new CyutCourse($depCode);
+ * $result = $cyut->crawlingDepartmentCourses(104, 2, 'TJ9');
+ * var_dump($result);
+ */
 
 /*
  * If you would crawling courses into your database,
